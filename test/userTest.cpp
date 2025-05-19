ec06531862da5fa4c52bf1fc3fdd641b07427397
gujial <gu18647403665@outlook.com>
2025-05-19 10:17:52 +0800

    test: 增加测试程序

---
+++ test/userTest.cpp
@@ -0,0 +1,18 @@
+//
+// Created by gujial on 25-5-19.
+//
+
+#include <QApplication>
+#include "controller.h"
+
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
+    controller * c = new controller(
+        "localhost",
+        "login",
+        "root",
+        "041109"
+    );
+
+    c->userRegister("administrator", "administrator", "123@123.123", 0);
+}
